c2a51c5d1c4041034aea54e8d96e04ce15709e20
Vyara Radeva <vyara.radomirova@gmail.com>
2026-04-02 21:30:28 +0300

    Initial Angular project setup

---
+++ src/app/create/create.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create',
+  imports: [],
+  templateUrl: './create.html',
+  styleUrl: './create.css',
+})
+export class Create {}
---
+++ src/app/navigation/navigation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [],
+  templateUrl: './navigation.html',
+  styleUrl: './navigation.css',
+})
+export class Navigation {}
