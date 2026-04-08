c7e20b592b2c8f2f024b3e07cbc4626d6295bfa0
Vyara Radeva <vyara.radomirova@gmail.com>
2026-04-08 21:12:54 +0300

    Add register design

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';   
+import { RouterLink } from '@angular/router';  
 
 @Component({
   selector: 'app-register',
-   standalone: true,
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterLink],   
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css',
+  styleUrls: ['./register.component.css']  
 })
 export class RegisterComponent {}
