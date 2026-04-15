593de2fa56625ca136eb38409dfd8bcbcb27373d
Vyara Radeva <vyara.radomirova@gmail.com>
2026-04-15 22:37:55 +0300

    Add conditional buttons in details page based on user role

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ProductService, Product } from '../services/product.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-details',
@@ -14,17 +15,30 @@ export class DetailsComponent implements OnInit {
   product: Product | null = null;
   isLoading: boolean = true;
   errorMessage: string = '';
+  isLoggedIn: boolean = false;
+  isAdmin: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
+    private authService: AuthService,
     private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
+    this.authService.user$.subscribe(user => {
+      this.isLoggedIn = !!user;
+      this.isAdmin = user?.email === 'admin@abv.bg';
+      this.cdr.detectChanges();
+    });
+
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.loadProduct(id);
+    } else {
+      this.isLoading = false;
+      this.errorMessage = 'Невалиден идентификатор на продукт';
+      this.cdr.detectChanges();
     }
   }
 
@@ -44,5 +58,17 @@ export class DetailsComponent implements OnInit {
         this.cdr.detectChanges();
       }
     });
+  }
+
+  addToCart(): void {
+    console.log('Добавяне в кошницата:', this.product);
+  }
+
+  editProduct(): void {
+    console.log('Редактиране на продукт:', this.product);
+  }
+
+  deleteProduct(): void {
+    console.log('Изтриване на продукт:', this.product);
   }
 }
